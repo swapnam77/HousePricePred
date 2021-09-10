95781be76f8410552e2e627cb33630fcbd86dd84
swapnam77 <86954031+swapnam77@users.noreply.github.com>
2021-09-10 11:06:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,32 @@ my_producer = KafkaProducer(
     bootstrap_servers = ['localhost:9092'],  
     value_serializer = lambda x:dumps(x).encode('utf-8')  
     )  
+
+kubectl create deployment --image=cnfldemos/orders-service:sha-844f538
+    apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: orders-service
+spec:
+  replicas: 1
+  containers:
+    - name: orders-service
+      image: cnfldemos/orders-service:sha-844f538
+            kubectl apply -f orders-service-deployment.yaml
+            function common::get_config() {
+  cat <<EOF
+configVersion: v1
+kubernetes:
+- name: ConnectConfigMapMonitor
+  apiVersion: v1
+  kind: ConfigMap
+  executeHookOnEvent: ["Added","Deleted","Modified"]
+  labelSelector:
+	matchLabels:
+  	  destination: ccloud
+  namespace:
+	nameSelector:
+  	  matchNames: ["default"]
+  jqFilter: ".data"
+EOF
+}
